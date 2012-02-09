f4bbbf4d9f6c644dbf6af24f158ac8215f4bed75
Alexander Kahoun <alexkahoun@gmail.com>
2012-02-08 19:53:36 -0700

    Initial check-in of source and base NuGet package

---
+++ src/content/Controllers/ErrorController.cs.pp
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace $rootnamespace$
+{
+	public class ErrorController :Controller
+	{
+		public RedirectToRouteResult Index()
+		{
+			return RedirectToAction("NotFoundError");
+		}
+
+		public ViewResult NotFoundError()
+		{
+			return View();
+		}
+	}
+}
